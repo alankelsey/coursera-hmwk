441ab6956c5f6f392636d3340d898ba13257ec1b
kunningklown <kunningklown@gmail.com>
2016-09-22 23:58:52 -0600

    update index

--- app.js
+++ app.js
@@ -4,7 +4,53 @@
 angular.module('myFirstApp', [])
 
 .controller('MyFirstController', function ($scope) {
-  $scope.name = "alan";
+  
+  $scope.lunchList = "";
+  $scope.response = "";
+
+  $scope.splitString = function(stringIn) {
+
+    var splitCount = stringIn.split(',')
+    var length = splitCount.length;
+    $scope.name = stringIn[i]
+    //splitCount.splice(0 , length);
+
+    for (var i =0; i < length; i++) {
+
+      if (stringIn[i] == null ) {
+        return  0;
+
+      } else {
+        return length;
+      //  return stringIn;
+      }
+
+    }
+
+
+  };
+
+  $scope.result = function (myList) {
+
+    var count = $scope.splitString($scope.lunchList)
+
+
+
+    if (count > 0 && count <= 3){
+         $scope.response = "Enjoy!";
+
+    }else if (count > 3) {
+         $scope.response = "Too Much!";
+
+    }else {
+
+        $scope.response =  "Please enter data first"
+
+    }
+
+
+
+  };
 
 });
 
